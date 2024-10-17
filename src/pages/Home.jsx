1af62d96f13e02239438d1ff2bd2a82a057699fa
soumens12 <101258865+soumens12@users.noreply.github.com>
2024-10-17 12:28:55 +0530

    homepage changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Search from '../component/Search';
 import PropertList from '../component/PropertList';
 import PropertyType from '../component/PropertyType';
 import PropertyAgent from '../component/PropertyAgent';
+import Testimonial from './Testimonial';
 
 function Home() {
   return (
@@ -95,52 +96,7 @@ function Home() {
 
 
         {/* <!-- Testimonial Start --> */}
-        <div className="container-xxl py-5">
-            <div className="container">
-                <div className="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style={{maxWidth: "600px"}}>
-                    <h1 className="mb-3">Our Clients Say!</h1>
-                    <p>Eirmod sed ipsum dolor sit rebum labore magna erat. Tempor ut dolore lorem kasd vero ipsum sit eirmod sit. Ipsum diam justo sed rebum vero dolor duo.</p>
-                </div>
-                <div className="owl-carousel testimonial-carousel wow fadeInUp" data-wow-delay="0.1s">
-                    <div className="testimonial-item bg-light rounded p-3">
-                        <div className="bg-white border rounded p-4">
-                            <p>Tempor stet labore dolor clita stet diam amet ipsum dolor duo ipsum rebum stet dolor amet diam stet. Est stet ea lorem amet est kasd kasd erat eos</p>
-                            <div className="d-flex align-items-center">
-                                <img className="img-fluid flex-shrink-0 rounded" src="assets/assets/img/testimonial-1.jpg" alt='testimonial' style={{width: "45px", height: "45px"}} />
-                                <div className="ps-3">
-                                    <h6 className="fw-bold mb-1">Client Name</h6>
-                                    <small>Profession</small>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="testimonial-item bg-light rounded p-3">
-                        <div className="bg-white border rounded p-4">
-                            <p>Tempor stet labore dolor clita stet diam amet ipsum dolor duo ipsum rebum stet dolor amet diam stet. Est stet ea lorem amet est kasd kasd erat eos</p>
-                            <div className="d-flex align-items-center">
-                                <img className="img-fluid flex-shrink-0 rounded" src="assets/assets/img/testimonial-2.jpg" alt='testimonial' style={{width: "45px", height: "45px"}} />
-                                <div className="ps-3">
-                                    <h6 className="fw-bold mb-1">Client Name</h6>
-                                    <small>Profession</small>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="testimonial-item bg-light rounded p-3">
-                        <div className="bg-white border rounded p-4">
-                            <p>Tempor stet labore dolor clita stet diam amet ipsum dolor duo ipsum rebum stet dolor amet diam stet. Est stet ea lorem amet est kasd kasd erat eos</p>
-                            <div className="d-flex align-items-center">
-                                <img className="img-fluid flex-shrink-0 rounded" src="assets/assets/img/testimonial-3.jpg" alt='testimonial' style={{width: "45px", height: "45px"}} />
-                                <div className="ps-3">
-                                    <h6 className="fw-bold mb-1">Client Name</h6>
-                                    <small>Profession</small>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <Testimonial></Testimonial>
         {/* <!-- Testimonial End --> */}
         
 
